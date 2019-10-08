ccdb025d2cd6c623a5219897ca6d54f8aaaf3bbc
nhopfe <55861853+nhopfe@users.noreply.github.com>
2019-10-08 13:00:12 -0600

    first daily exercise

---
+++ src/javascript/daily.test.js
@@ -0,0 +1,36 @@
+/*	
+	Write the function that will create this output:
+
+*** the two values are not the same:
+    p1--> a
+    p2--> b
+*** the two values are not the same:
+    p1--> 1
+    p2--> 2
+*** the two values are not the same:
+    p1--> 2
+    p2--> 2
+*/
+
+// Write the function after this comment ---
+
+import {assertEquals} from './daily'
+
+test('assertEquals', () => {
+    expect(assertEquals("a","b")).toBe(false);
+    expect(assertEquals("a","a")).toBe(true);
+    expect(assertEquals(1,2)).toBe(false);
+    expect(assertEquals(2,2)).toBe(true);
+    expect(assertEquals("2",2)).toBe(false);
+    expect(assertEquals("This value","This value")).toBe(true);
+});
+
+
+// and before this comment ---
+
+// assertEquals("a","b");
+// assertEquals("a","a");
+// assertEquals(1,2);
+// assertEquals(2,2);
+// assertEquals("2",2);
+// assertEquals("This value","This value");
