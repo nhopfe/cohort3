bcf75768b036e5fd6f02a345af674b7e290a5b2d
nhopfe <55861853+nhopfe@users.noreply.github.com>
2019-10-09 13:36:35 -0600

    progress on Syntax exercise

--- src/01-getting-started/scripts/syntax.js
+++ src/01-getting-started/scripts/syntax.js
@@ -1,5 +1,4 @@
 // define attributes / variables
-    // number
 const functions = {
     
     testForAttributeType: (p1) => {
@@ -9,7 +8,59 @@ const functions = {
     testForArrays: (p1) => {
         let p2 = Array.isArray(p1);
         return p2;
+    },
+    testForEqualValue: (p1) => {
+        let mango = new Object ();
+        mango.color = "yellow";
+        if (p1 === mango.color) return true;
+        return false;
+    },
+    testIfVariableIsUndefined: (p1) => {
+        let p2 = typeof(p1);
+        if (p2 === "undefined") return true;
+        return false;
+    },
+    unshiftOnlyStringsToArray: (p1) => {
+        let testUnshiftArray = ["this", "is", "a", "test", "array"];
+        let p2 = typeof(p1);
+        if (p2 === "string") {testUnshiftArray.unshift(p1); return "unshifted"};
+        return "not a string";
+    },
+    pushOnlyNumbersToArray: (p1) => {
+        let testPushArray = [3, 4, 5];
+        let p2 = typeof(p1);
+        if (p2 === "number") {testPushArray.push(p1); return "pushed"};
+        return "not a number";
+    },
+    changeFirstValueInArrayIfString: (p1) => {
+        let testChangeArray = ["Sam", "Fred", "Susy"];
+        let p2 = typeof(p1);
+        if (p2 === "string") {testChangeArray[0] = p1; return "changed"};
+        return "not a string";
+    },
+    changeLoopConditionIfNumber: (p1) => {
+        let p2 = typeof(p1);
+        let items = 0;
+        let i;
+        if (p2 === "number" && p1 > 0) {for (i = 0; i < p1; i++) {items[i]}; return "condition changed"};
+        if (p2 === "number" && p1 <= 0) return "number must be greater than 0";
+        return "not a number";
+    },
+    deconstructingAnObjectWithForInLoop: (obj) => {
+        let allObjectData = "";
+        for (let key in obj) {
+            allObjectData += `Key: ${key}, Value: ${obj[key]}\n`;
+        }
+        return allObjectData;
+    },
+    countingDownToZeroWithWhileLoop: (num) => {
+        if (num <= 10 && num > 0) {
+            let total = 0;
+            let i = num;
+            while (i > 0) {total += i; i--;}; return total;
+        return "number is not between 1 and 10";
     }
+
 };
     
 export default functions;
--- src/01-getting-started/scripts/syntax.test.js
+++ src/01-getting-started/scripts/syntax.test.js
@@ -1,24 +1,58 @@
 import functions from './syntax'
-// define attributes / variables
-    test('what type is attribute', () => {
+    test('what data type is attribute', () => {
         expect(functions.testForAttributeType(1)).toBe("number");
         expect(functions.testForAttributeType("test")).toBe("string");
         expect(functions.testForAttributeType(true || false)).toBe("boolean");
         expect(functions.testForArrays([])).toBe(true);
+        expect(functions.testForArrays({})).toBe(false);
     });
-    // dictionary / objects
-    // undefined
-// sample if / else
-// functions
-    // parameters
-    // returns
-// arrays
-    // add to the front
-    // add to the end
-    // update values
+
+    test('is the value "yellow" inside this object?', () => {
+        expect(functions.testForEqualValue("yellow")).toBe(true);
+        expect(functions.testForEqualValue("orange")).toBe(false);
+        expect(functions.testForEqualValue(true)).toBe(false);
+    });
+
+    test('is the attribute variable undefined?', () => {
+        let theFoo
+        expect(functions.testIfVariableIsUndefined(1)).toBe(false);
+        expect(functions.testIfVariableIsUndefined(theFoo)).toBe(true);
+    });
+
+    test('only unshift array if attribute is a string', () => {
+        expect(functions.unshiftOnlyStringsToArray("1")).toBe("unshifted");
+        expect(functions.unshiftOnlyStringsToArray(1)).toBe("not a string");
+    });
+
+    test('only push array if attribute is a number', () => {
+        expect(functions.pushOnlyNumbersToArray(1)).toBe("pushed");
+        expect(functions.pushOnlyNumbersToArray("1")).toBe("not a number");
+    });
+
+    test('only change first value in array if attribute is a string', () => {
+        expect(functions.changeFirstValueInArrayIfString("1")).toBe("changed");
+        expect(functions.changeFirstValueInArrayIfString(1)).toBe("not a string");
+    });
+
+    test('use a number to change loop condition', () => {
+        expect(functions.changeLoopConditionIfNumber(1)).toBe("condition changed");
+        expect(functions.changeLoopConditionIfNumber(0)).toBe("number must be greater than 0");
+        expect(functions.changeLoopConditionIfNumber("1")).toBe("not a number");
+    });
+//this one still needs some work.
+    test('deconstructing an object using a for/in loop', () => {
+        const pets = {dog: "Shiloh", cat: "Zoe", horse: "Ben"};
+        expect(functions.deconstructingAnObjectWithForInLoop(pets)).toBe(`Key: dog, Value: Shiloh\nKey: cat, Value: Zoe\nKey: horse, Value: Ben\n`);
+        expect(functions.deconstructingAnObjectWithForInLoop(5)).toBe("");
+    });
+
+    test('counting down to zero with while loop', () => {
+        expect(functions.deconstructingAnObjectWithForInLoop(11)).toBe(
+        expect(functions.deconstructingAnObjectWithForInLoop("15")).toBe("");
+        expect(functions.deconstructingAnObjectWithForInLoop(10)).toBe(
+    });
+
 // loops 
-    // for
-    // for/in
     // while
     // do while
     // forEach (with array and function)
