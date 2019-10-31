8e78d1c77b021011e3fb8c861a7d847a36ae3dff
nhopfe <55861853+nhopfe@users.noreply.github.com>
2019-10-31 16:43:15 -0600

    created fetch files for 920

---
+++ src/03-objects/scripts/fetch.js
@@ -0,0 +1,7 @@
+const fetchFuncs = {
+    dummy: () => {
+        console.log('hello from fetch.js');
+    }
+}
+
+export default fetchFuncs;
---
+++ src/03-objects/scripts/fetch.test.js
@@ -0,0 +1,6 @@
+import fetchFuncs from './fetch.js'
+
+test('testing dummy function', () => {
+    console.log('hello from fetch.test.js');
+    fetchFuncs.dummy();
+})
