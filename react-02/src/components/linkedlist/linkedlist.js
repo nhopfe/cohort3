5210857515e093fdaa2111d6c3027020575c20ec
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-01-02 13:55:04 -0700

    Got POJO and tests functioning for linked list

---
+++ react-02/src/components/linkedlist/linkedlist.js
@@ -0,0 +1,157 @@
+export class ListNode {
+
+    constructor(subject, amount) {
+        this.subject = subject;
+        this.amount = Number(amount);
+        this.forwardNode = null;
+        this.backwardNode = null;
+    }
+
+    show() { 
+        return `Current Item: ${this.subject}, Current Amount: ${this.amount}.`
+    }
+};
+
+export class LinkedList {
+
+    // -- Chose Doubly-LinkedList --- 
+    // Reasoning:   Doubly-liked allows searches in both directions, imroving both
+    //              functionality and performance. 
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.current = null;
+    }
+
+    insert(subject, amount) { 
+        let newListNode = new ListNode(subject, amount);
+        if (!this.current) {
+            this.head = newListNode;
+            this.tail = newListNode;
+            this.current = newListNode;
+            return newListNode;
+        }
+        else {
+            let newNext = this.current.next;
+            let newPrev = this.current;
+            if (this.current === this.tail) {
+                this.current.forwardNode = newListNode;
+                newListNode.backwardNode = newPrev;
+                this.tail = newListNode;
+                this.current = newListNode;
+                newListNode.forwardNode = null;
+                return newListNode;
+            }
+            else {
+                this.current.forwardNode = newListNode;
+                newListNode.backwardNode = newPrev;
+                newListNode.forwardNode = newNext;
+                newNext.backwardNode = newListNode;
+                this.current = newListNode;
+                return newListNode;
+            }
+        }
+    }
+
+    delete
+    (node) {
+        if (!node) {
+            return null
+        }
+        else {
+            let newNext = node.forwardNode;
+            let newPrev = node.backwardNode;
+            if (node === this.head && node === this.tail) {
+                this.head = null;
+                this.tail = null;
+                this.current = null;
+                node = null;
+                return "";
+            }
+            if (node === this.head) {
+                this.head = newNext;
+                this.head.backwardNode = null;
+                this.current = this.head;
+                node = null;
+                return this.head;
+            }
+            if (node === this.tail) {
+                this.tail = newPrev;
+                this.tail.forwardNode = null;
+                this.current = this.tail;
+                node = null;
+                return this.tail;
+            }
+            else {
+                let newCurrentNode = newPrev;
+                newCurrentNode.forwardNode = newNext;
+                newNext.backwardNode = newCurrentNode;
+                this.current = newCurrentNode;
+                node = null;
+                return newCurrentNode;
+            }
+        }
+    }
+
+    first() {
+        if (!this.head) {
+            return null;
+        }
+        this.current = this.head;
+        return this.head;
+    }
+
+    last() {
+        if (!this.tail) {
+            return null;
+        }
+        this.current = this.tail;
+        return this.tail;
+    }
+
+    next(node) {
+        if (!node) {
+            return null;
+        }
+        else if (!node.forwardNode) {
+            this.current = node;
+            return node;
+        }
+        else {
+            let nextNode = node.forwardNode;
+            this.current = nextNode;
+            return nextNode;
+        }
+    }
+
+    previous(node) {
+        if (!node) {
+            return null;
+        }
+        else if (!node.backwardNode) {
+            this.current = node;
+            return node;
+        }
+        else {
+            let prevNode = node.backwardNode;
+            this.current = prevNode;
+            return prevNode;
+        }
+    }
+
+    total() {
+        if (!this.head) {
+            return 0;
+        }
+        else {
+            let currentNode = this.head;
+            let totalNumber = 0;
+            while (currentNode) {
+                totalNumber += currentNode.amount;
+                currentNode = currentNode.forwardNode;
+            }
+            return totalNumber;
+        }
+    }
+};
---
+++ react-02/src/components/linkedlist/linkedlist.test.js
@@ -0,0 +1,65 @@
+import { ListNode, LinkedList } from './linkedlist';
+
+test('test ListNode show', () => {
+    const testListNode = new ListNode("Test Node", 1);
+    expect(testListNode.show()).toBe("Current Item: Test Node, Current Amount: 1.");
+});
+
+test('test LinkedList insert', () => {
+    const testLinkedList = new LinkedList();
+    const testNodeOne = testLinkedList.insert("Test Node One", 1);
+    expect(testNodeOne).toBe(testLinkedList.head);
+    expect(testNodeOne).toBe(testLinkedList.tail);
+    expect(testNodeOne).toBe(testLinkedList.current);
+    const testNodeTwo = testLinkedList.insert("Test Node Two", 2);
+    expect(testNodeTwo).toBe(testLinkedList.tail);
+    expect(testNodeTwo).toBe(testLinkedList.current);
+    expect(testNodeTwo.forwardNode).toBe(null);
+    expect(testNodeTwo.backwardNode).toBe(testNodeOne);
+    const testNodeThree = testLinkedList.insert("Test Node Three", 3);
+    expect(testNodeThree).toBe(testLinkedList.tail);
+    expect(testNodeThree).toBe(testLinkedList.current);  
+    expect(testNodeThree.forwardNode).toBe(null);
+    expect(testNodeThree.backwardNode).toBe(testNodeTwo);
+});
+
+test('test LinkedList delete', () => {
+    const testLinkedList = new LinkedList();
+    expect(testLinkedList.delete()).toBe(null);
+    const testNodeOne = testLinkedList.insert("Test Node One", 1);
+    expect(testLinkedList.delete(testNodeOne)).toBe("");
+    const testNodeTwo = testLinkedList.insert("Test Node Two", 2);
+    const testNodeThree = testLinkedList.insert("Test Node Three", 3);
+    expect(testLinkedList.delete(testNodeTwo)).toBe(testNodeThree);
+    expect(testLinkedList.head).toBe(testNodeThree);
+    expect(testLinkedList.tail).toBe(testNodeThree);
+    expect(testLinkedList.current).toBe(testNodeThree);
+    expect(testNodeThree.forwardNode).toBe(null);
+    expect(testNodeThree.backwardNode).toBe(null);
+});
+
+test('test LinkedList first, previous, next, last functions', () => {
+    const testLinkedList = new LinkedList();
+    expect(testLinkedList.first()).toBe(null);
+    expect(testLinkedList.last()).toBe(null);
+    expect(testLinkedList.previous()).toBe(null);
+    expect(testLinkedList.next()).toBe(null);
+    const testNodeOne = testLinkedList.insert("Test Node One", 1);
+    const testNodeTwo = testLinkedList.insert("Test Node Two", 2);
+    const testNodeThree = testLinkedList.insert("Test Node Three", 3);
+    expect(testLinkedList.first()).toBe(testNodeOne);
+    expect(testLinkedList.last()).toBe(testNodeThree);
+    expect(testLinkedList.previous(testNodeThree)).toBe(testNodeTwo);
+    expect(testLinkedList.next(testNodeTwo)).toBe(testNodeThree);
+});
+
+test('test LinkedList total', () => {
+    const testLinkedList = new LinkedList();
+    expect(testLinkedList.total()).toBe(0);
+    testLinkedList.insert("Test Node One", 1);
+    expect(testLinkedList.total()).toBe(1);
+    testLinkedList.insert("Test Node Two", 10);
+    expect(testLinkedList.total()).toBe(11);
+    testLinkedList.delete(testLinkedList.head);
+    expect(testLinkedList.total()).toBe(10);
+});
