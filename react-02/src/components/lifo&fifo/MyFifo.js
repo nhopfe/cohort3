d67f6a5cfcc4916db9b167d08ac3d03a05447ad7
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-01-04 18:14:25 -0700

    first attempts at highlighting new items

--- react-02/src/components/lifo&fifo/MyFifo.js
+++ react-02/src/components/lifo&fifo/MyFifo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './lifofifo.css';
 
 const MyFifo = (props) => {
     const fifoDisplay = props.fifo.list.map((item, i) => {
         return (
-            <div className={`fifo-card` + ((item === props.lastAdded) ? "last-added-fifo-card" : null)} key={i}>
+            <div className={`fifo-card` + ((item === props.fifoLastAddedItem) ? "last-added-fifo-card" : null)} key={i}>
                 <span className="fifo-card-text">{item}</span>
             </div>
         )
--- react-02/src/components/lifo&fifo/MyLifo.js
+++ react-02/src/components/lifo&fifo/MyLifo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './lifofifo.css';
 
 const MyLifo = (props) => {
     const lifoDisplay = props.lifo.list.map((item, i) => {
         return (
-            <div className={`lifo-card` + ((item === props.nextInLine) ? "next-lifo-card" : null)} key={i}>
+            <div className={`lifo-card` + ((item === props.lifoNextItem) ? "last-added-lifo-card" : null)} key={i}>
                 <span className="lifo-card-text">{item}</span>
             </div>
         )
