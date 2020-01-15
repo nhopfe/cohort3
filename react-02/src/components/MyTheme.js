19bf00a0f98631e0c60b29e098a093e9dd065ad7
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-01-15 10:49:13 -0700

    removed comment

--- react-02/src/components/MyTheme.js
+++ react-02/src/components/MyTheme.js
@@ -3,7 +3,6 @@ import React from 'react';
 export const themes = {
     gradient: {
         background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, #282c34 15%, #09d3ac 100%)',
-        // background: 'blue',
     },
     solid: {
         background: '#282c34',
